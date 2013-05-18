41621a983a48b4ca7e12bcaadb1646d890065de3
Jim <jarosser06@gmail.com>
2013-05-18 13:30:19 -0400

    added test cookbook

---
+++ test/cookbooks/vagrant_test/README.md
@@ -0,0 +1,9 @@
+Vagrant Test Cookbook
+================
+Vagrant Test
+
+License and Authors
+-------------------
+Authors: Jim Rosser 
+
+License: MIT
---
+++ test/cookbooks/vagrant_test/metadata.rb
@@ -0,0 +1,9 @@
+name             'vagrant_test'
+maintainer       'Jim Rosser'
+maintainer_email 'jarosser06@arch.tamu.edu'
+license          'MIT'
+description      'Installs/Configures vagrant_test'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'vagrant'
